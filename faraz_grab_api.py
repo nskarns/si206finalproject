f5889ebdb142491d38d5af3c6ce882d3738a4825
farazali8 <farazali@umich.edu>
2023-12-03 18:44:59 -0500

    Api Grab

--- faraz_grab_api.py
+++ faraz_grab_api.py
@@ -21,39 +21,32 @@ for matchday in range(1, 15):
     querystring = {"matchday": str(matchday)}
     response = requests.get(url, headers=headers, params=querystring)
 
-    try:
-        response.raise_for_status()
-        data = response.json()
 
-        for match in data.get('matches', []):
-            team1_name = match['team1']['teamName']
-            team2_name = match['team2']['teamName']
-            team1_score = match['team1'].get('teamScore', None)
-            team2_score = match['team2'].get('teamScore', None)
+response.raise_for_status()
+data = response.json()
+
+for match in data.get('matches', []):
+    team1_name = match['team1']['teamName']
+    team2_name = match['team2']['teamName']
+    team1_score = match['team1'].get('teamScore', None)
+    team2_score = match['team2'].get('teamScore', None)
 
             # Determine the winner or mark as draw
-            if team1_score is not None and team2_score is not None:
-                if team1_score > team2_score:
-                    winner = team1_name
-                elif team2_score > team1_score:
-                    winner = team2_name
-                else:
-                    winner = "Draw"
-            else:
-                winner = None
+    if team1_score is not None and team2_score is not None:
+        if team1_score > team2_score:
+            winner = team1_name
+        elif team2_score > team1_score:
+            winner = team2_name
+        else:
+                winner = "Draw"
+    else:
+        winner = None
 
             # Store match information in the Matches table
-            cur.execute("INSERT INTO Matches VALUES (?, ?, ?, ?, ?, ?)", (matchday, team1_name, team2_name, team1_score, team2_score, winner))
+    cur.execute("INSERT INTO Matches VALUES (?, ?, ?, ?, ?, ?)", (matchday, team1_name, team2_name, team1_score, team2_score, winner))
 
         # Commit changes to the database after each matchday
-        conn.commit()
-
-    except requests.exceptions.HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
-        print(f'Response text: {response.text}')
-
-    except Exception as e:
-        print(f'An error occurred: {e}')
+conn.commit()
 
 # Close the database connection
 conn.close()
