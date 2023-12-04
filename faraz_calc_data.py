89ed6663a2cd7cf424e4b725ee4544128e1d5371
farazali8 <farazali@umich.edu>
2023-12-03 19:18:22 -0500

    Fixed errors

--- faraz_calc_data.py
+++ faraz_calc_data.py
@@ -6,40 +6,49 @@ def calculate_average_goals(database_path):
     conn = sqlite3.connect(database_path)
     cur = conn.cursor()
 
-    try:
-        # Create a dictionary to store total goals and match counts for each team
-        team_stats = {}
+    
+    # Create a dictionary to store total goals and match counts for each team
+    team_stats = {}
 
-        # Query to get total goals and match counts for each team
-        query = '''
-                SELECT team_name, 
-                       SUM(total_goals) AS total_goals, 
-                       COUNT(*) AS match_count
-                FROM (
-                    SELECT team1_name AS team_name, team1_score AS total_goals FROM Matches
-                    UNION ALL
-                    SELECT team2_name AS team_name, team2_score AS total_goals FROM Matches
-                )
-                GROUP BY team_name
-                '''
+    # Query to get total goals and match counts for each team
+    query = '''
+            SELECT team_name, 
+                   SUM(total_goals) AS total_goals, 
+                   COUNT(*) AS match_count
+            FROM (
+                SELECT team1_name AS team_name, team1_score AS total_goals FROM Matches
+                UNION ALL
+                SELECT team2_name AS team_name, team2_score AS total_goals FROM Matches
+            )
+            GROUP BY team_name
+            '''
 
-        cur.execute(query)
-        rows = cur.fetchall()
+    cur.execute(query)
+    rows = cur.fetchall()
 
         # Populate the team_stats dictionary with team_name as key
         # and total goals, match count as values
-        for row in rows:
-            team_name, total_goals, match_count = row
-            team_stats[team_name] = {'total_goals': total_goals, 'match_count': match_count}
+    for row in rows:
+        team_name, total_goals, match_count = row
+        team_stats[team_name] = {'total_goals': total_goals, 'match_count': match_count}
 
-        return team_stats
+    with open('average_goals_data.txt', 'w') as file:
+        for team, stats in team_stats.items():
+           # Get the average goals for the team
+            average_goals = stats['total_goals'] / stats['match_count']
 
-    except Exception as e:
-        print(f'An error occurred: {e}')
+            # Create a formatted string with the team name and average goals
+            formatted_string = f"{team}: Average Goals - {average_goals:.2f}\n"
 
-    finally:
-        # Close the database connection
-        conn.close()
+            # Write the formatted string to the file
+            file.write(formatted_string)
+            
+            #print(average_goals)
+
+    
+    return team_stats
+
+    conn.close()
 
 
 #####################################################################################################################################
@@ -49,22 +58,23 @@ def create_average_goals_chart(database_path):
     # Call the calculate_average_goals function
     team_stats = calculate_average_goals(database_path)
 
-    try:
+    
         # Check if team_stats is not empty before creating the chart
-        if team_stats:
+    if team_stats:
             # Create a bar chart
-            plt.bar(team_stats.keys(), [stats['total_goals'] / stats['match_count'] for stats in team_stats.values()])
-            plt.xlabel('Team')
-            plt.ylabel('Average Goals')
-            plt.title('Average Goals per Team')
-            plt.xticks(rotation=45, ha='right')
-            plt.show()
+        plt.bar(team_stats.keys(), [stats['total_goals'] / stats['match_count'] for stats in team_stats.values()])
+        plt.xlabel('Team')
+        plt.ylabel('Average Goals')
+        plt.title('Average Goals per Team')
+        plt.xticks(rotation=45, ha='right')
+        plt.show()
+           
+        plt.savefig('average_goals_chart.png')
+    
+    else:
+        print("No data available for the chart.")
 
-        else:
-            print("No data available for the chart.")
 
-    except Exception as e:
-        print(f'An error occurred: {e}')
-
-# Example usage
+#FunctionCall
 create_average_goals_chart("premier_league_data.db")
+#print(calculate_average_goals("premier_league_data.db"))
